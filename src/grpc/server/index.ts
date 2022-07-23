dc9e2875f8e637522196c26708b090f05d44e060
Rodrigo Silveira <r3850365@gmail.com>
2022-07-23 16:57:30 -0700

    Implemented an architecture to handle data based on the environemnt (dev, prod, e2e)

--- src/grpc/server/index.ts
+++ src/grpc/server/index.ts
@@ -1,9 +1,21 @@
+import { resolve } from "path";
+const config =  require('../../../config/config');
+const absolutePATH = resolve('');
+process.env.CLUB_MEMBERS_RAW_FN = absolutePATH + resolve(config.CLUB_MEMBERS_RAW_FN);
+console.log(`process.env.CLUB_MEMBERS_RAW_FN = ${process.env.CLUB_MEMBERS_RAW_FN}`);
+process.env.CLUB_MEMBERS_EDITED_FN = absolutePATH + resolve(config.CLUB_MEMBERS_EDITED_FN);
+console.log(`process.env.CLUB_MEMBERS_EDITED_FN = ${process.env.CLUB_MEMBERS_EDITED_FN}`);
+process.env.TOURNAMENTS_RAW_FN = absolutePATH + resolve(config.TOURNAMENTS_RAW_FN);
+console.log(`process.env.TOURNAMENTS_RAW_FN = ${process.env.TOURNAMENTS_RAW_FN}`);
+process.env.TOURNAMENTS_EDITED_FN = absolutePATH + resolve(config.TOURNAMENTS_EDITED_FN);
+console.log(`process.env.TOURNAMENTS_EDITED_FN = ${process.env.TOURNAMENTS_EDITED_FN}`);
+
 import { Server, ServerCredentials } from "@grpc/grpc-js";
 import { SClubMemberService } from "../proto/club-member_grpc_pb";
+
 import { ClubMemberServices } from "./club-member-services";
 import {STournamentService} from "../proto/tournament_grpc_pb";
 import {TournamentServices} from "./tournament-services";
-
 
 const server = new Server();
 // TODO Find a way to remove this ts-ignore
@@ -12,7 +24,7 @@ server.addService(SClubMemberService, new ClubMemberServices());
 // @ts-ignore TODO find a way to remove this
 server.addService(STournamentService, new TournamentServices());
 
-const port = 50051;
+const port = config.GRPC_PORT;
 const uri = `localhost:${port}`;
 console.log(`Listening on ${uri}`);
 server.bindAsync(
